d24fbc9a58569692448d6297c07c386e0208fcb3
Chukwudi Eze <chukwudieze97@gmail.com>
2026-08-14 08:19:57 +0100

    feat(extension-browser): implement ExtensionBrowserExecutor and transport interfaces

---
+++ apps/server/src/resources/tools/executors/extension-browser-transport.ts
@@ -0,0 +1,26 @@
+import type {
+  BrowserToolExecutionContext,
+  BrowserToolName,
+} from '../types/browser-tool.types';
+
+export const EXTENSION_BROWSER_TRANSPORT = Symbol(
+  'EXTENSION_BROWSER_TRANSPORT',
+);
+
+export interface ExtensionBrowserCommand {
+  readonly commandId: string;
+  readonly runId: string;
+  readonly name: BrowserToolName;
+  readonly input: Readonly<Record<string, unknown>>;
+}
+
+/**
+ * Sends a command to the extension connection owned by a user/browser session.
+ * Implementations are responsible for correlation, timeouts, and disconnection.
+ */
+export interface ExtensionBrowserTransport {
+  send<TResult>(
+    context: BrowserToolExecutionContext,
+    command: ExtensionBrowserCommand,
+  ): Promise<TResult>;
+}
--- apps/server/src/resources/tools/tools.module.ts
+++ apps/server/src/resources/tools/tools.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ToolsService } from './tools.service';
+import { ExtensionBrowserExecutor } from './executors/extension-browser.executor';
+import { BROWSER_TOOL_EXECUTOR } from './types/browser-tool.types';
 
 @Module({
-  providers: [ToolsService],
+  providers: [
+    ExtensionBrowserExecutor,
+    {
+      provide: BROWSER_TOOL_EXECUTOR,
+      useExisting: ExtensionBrowserExecutor,
+    },
+    ToolsService,
+  ],
   exports: [ToolsService],
 })
 export class ToolsModule {}
